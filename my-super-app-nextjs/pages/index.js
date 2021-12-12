ff1e3e0c798656f5759289a3592a8834e25abce0
Carogd <carolinegaudiard@gmail.com>
2021-12-12 19:32:34 +0100

    lancement application Next JS - Test

---
+++ my-super-app-nextjs/pages/index.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { useState, useEffect } from 'react'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  const [todos, setTodos] = useState([])
+  useEffect(() => {
+    const data = fetch('https://jsonplaceholder.typicode.com/todos?_limit=4')
+    .then(r => r.json())
+    .then(data => {
+      setTodos(data)
+    })
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>Ma super todo</title>
+        <meta name="description" content="la super todo de Caroline" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ul>
+        {todos.map( todos => <li><p> {todos.title} </p></li>)}
+      </ul>
+     </>
+     
+  )
+}
+
